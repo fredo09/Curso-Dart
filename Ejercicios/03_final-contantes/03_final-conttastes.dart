c74aca4c457e5bcff7414112d7f0d9cd209bdfa2
Jose Alfredo Vazquez Jimenez <fredy@MacBook-Air-de-Jose.local>
2022-06-12 00:56:23 -0500

    Entendiendo const var y final en dart

---
+++ Ejercicios/03_final-contantes/03_final-conttastes.dart
@@ -0,0 +1,14 @@
+main() {
+  var a = 10;
+
+  //Usamos final y const para no cambiar el valor desde que se han inicializado
+  final b = 20;
+
+  const c = 20;
+
+  //desmostrando la diferencia entre final y const
+
+  final personas = ['Juan', 'Pablo', 'Mario'];
+
+  const personas2 = ['Juan', 'Pablo', 'Mario'];
+}
